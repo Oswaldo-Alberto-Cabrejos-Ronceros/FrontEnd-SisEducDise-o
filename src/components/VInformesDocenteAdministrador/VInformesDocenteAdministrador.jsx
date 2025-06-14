d043efda98e5c4c983771d430ae68acdbf9b4ca6
Oswaldo <U22209742@utp.edu.pe>
2025-06-14 13:32:44 -0500

    feat:Implementacion de tabs en VInformes Docente Administrador

--- src/components/VInformesDocenteAdministrador/VInformesDocenteAdministrador.jsx
+++ src/components/VInformesDocenteAdministrador/VInformesDocenteAdministrador.jsx
@@ -1,32 +1,74 @@
-import './VInformesDocenteAdministrador.css'
-import { Routes, Route, Link, Navigate } from 'react-router-dom';
-import PrimaryButton from '../generalsComponets/PrimaryButton/PrimaryButton';
-import VInformesDocenteAdministradorAuxiliar from './VInformesDocenteAdministradorAuxiliar/VInformesDocenteAdministradorAuxiliar'
-import VInformesDocenteAdministradorBimestral from './VInformesDocenteAdministradorBimestral/VInformesDocenteAdministradorBimestral'
+import "./VInformesDocenteAdministrador.css";
+import {
+  Routes,
+  Route,
+  NavLink,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
+import VInformesDocenteAdministradorAuxiliar from "./VInformesDocenteAdministradorAuxiliar/VInformesDocenteAdministradorAuxiliar";
+import VInformesDocenteAdministradorBimestral from "./VInformesDocenteAdministradorBimestral/VInformesDocenteAdministradorBimestral";
+import { Tabs } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 
 function VInformesDocenteAdministrador() {
+  //para indice activo
+  //para saber la ubicacion de la ruta
+  const location = useLocation();
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    if (location.pathname === "/administrador/informes/auxiliar") {
+      setActiveIndex(0);
+    } else if (location.pathname === "/administrador/informes/bimestral") {
+      setActiveIndex(1);
+    }
+  }, [location.pathname]);
   return (
-    <div className='VInformesDocenteAdministradorContainer' >
-        <div className='VInformesDocenteAdministradorTitleContainer'>
-            <h3>Informes</h3>
-        </div>
-        <div className='VInformesDocenteAdministradorButtonsContainer'>
-            <Link className='LinkVInformesDocenteAdministrador' to="auxiliar">
-            <PrimaryButton nombre="Auxiliar"/>
-            </Link>
-            <Link className='LinkVInformesDocenteAdministrador' to="bimestral">
-            <PrimaryButton nombre="Bimestral"/>
-            </Link>
-        </div>
-        <div className='CambVInformesDocenteAdministrador'>
-            <Routes>
-                <Route index element ={<Navigate to={"auxiliar"}/>}/>
-                <Route path='auxiliar' element={<VInformesDocenteAdministradorAuxiliar/>}/>
-                <Route path='bimestral' element={<VInformesDocenteAdministradorBimestral/>}/>
-            </Routes>
-        </div>
+    <div className="VInformesDocenteAdministradorContainer">
+      <div className="VInformesDocenteAdministradorTitleContainer">
+        <h3>Informes</h3>
+      </div>
+      <div className="VInformesDocenteAdministradorButtonsContainer">
+        <Tabs.Root
+          defaultValue={0}
+          index={activeIndex}
+          onChange={(index) => setActiveIndex(index)}
+        >
+          <Tabs.List>
+            <Tabs.Trigger
+              as={NavLink}
+              value={0}
+              to="/administrador/informes/auxiliar"
+            >
+              Auxiliar
+            </Tabs.Trigger>
+            <Tabs.Trigger
+              as={NavLink}
+              value={1}
+              to="/administrador/informes/bimestral"
+            >
+              Bimestral
+            </Tabs.Trigger>
+          </Tabs.List>
+        </Tabs.Root>
+      </div>
+      <div className="CambVInformesDocenteAdministrador">
+        <Routes>
+          <Route index element={<Navigate to={"auxiliar"} />} />
+          <Route
+            path="auxiliar"
+            element={<VInformesDocenteAdministradorAuxiliar />}
+          />
+          <Route
+            path="bimestral"
+            element={<VInformesDocenteAdministradorBimestral />}
+          />
+        </Routes>
+      </div>
     </div>
-  )
+  );
 }
 
-export default VInformesDocenteAdministrador
+export default VInformesDocenteAdministrador;
